04f256d3c506e107ca059ecc21ea10cf6da01550
Ralph Urlus <rurlus.dev@gmail.com>
2021-11-08 10:18:59 +0100

    ENH: Make cpp extension available under _core

---
+++ mmu/__init__.py
@@ -0,0 +1,5 @@
+from mmu.lib import _core
+import mmu.simulation as simulation
+import mmu.viz as viz
+
+__all__ = ["_core", "simulation", "viz"]
---
+++ mmu/lib/__init__.py
@@ -0,0 +1,4 @@
+import mmu.lib._mmu_core as _core
+
+__all__ = ["_core"]
+
