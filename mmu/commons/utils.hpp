81ab1399b50b8e8db275741fbcec222a42e65903
Ralph Urlus <rurlus.dev@gmail.com>
2021-11-15 14:39:30 +0100

    ENH: [CPP] Add equal shape check

--- mmu/commons/utils.hpp
+++ mmu/commons/utils.hpp
@@ -38,6 +38,22 @@ inline void check_contiguous(const py::array_t<T>& arr, const std::string& name)
     }
 }
 
+template <typename T, typename V>
+inline void check_equal_shape(const py::array_t<T>& x, const py::array_t<V>& y, const std::string& x_name, const std::string& y_name) {
+    size_t x_dim = x.ndim();
+    size_t y_dim = y.ndim();
+    bool pass = false;
+    if (x_dim == y_dim) {
+        for (size_t i = 0; i < x_dim; i++) {
+            pass = x.shape(i) == y.shape(i);
+        }
+    }
+    if (pass > 0) {
+        return;
+    }
+    throw std::runtime_error(x_name + " and " + y_name + " should have equal shape");
+}
+
 }  // namespace details
 }  // namespace mmu
 
