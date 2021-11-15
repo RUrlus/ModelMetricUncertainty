7b7ab9df952b0019b0df299234b20d6c2b2b4762
Ralph Urlus <rurlus.dev@gmail.com>
2021-11-15 14:44:09 +0100

    CHG: Update init modules

--- mmu/__init__.py
+++ mmu/__init__.py
@@ -1,5 +1,32 @@
-from mmu.lib import _core
+import mmu.metrics as metrics
 import mmu.simulation as simulation
 import mmu.viz as viz
 
-__all__ = ["_core", "simulation", "viz"]
+from mmu.metrics import binary_metrics
+from mmu.metrics import binary_metrics_proba
+from mmu.metrics import binary_metrics_confusion
+from mmu.metrics import binary_metrics_thresholds
+from mmu.metrics import binary_metrics_runs_thresholds
+from mmu.metrics import confusion_matrix
+from mmu.metrics import confusion_matrix_proba
+from mmu.metrics import confusion_matrix_to_dataframe
+from mmu.metrics import metrics_to_dataframe
+
+from mmu.simulation import ModelGenerator
+
+
+__all__ = [
+    'binary_metrics',
+    'binary_metrics_proba',
+    'binary_metrics_confusion',
+    'binary_metrics_thresholds',
+    'binary_metrics_runs_thresholds',
+    'confusion_matrix',
+    'confusion_matrix_proba',
+    'confusion_matrix_to_dataframe',
+    "simulation",
+    "viz",
+    "metrics",
+    "metrics_to_dataframe",
+    "ModelGenerator"
+]
---
+++ mmu/metrics/__init__.py
@@ -0,0 +1,25 @@
+from mmu.metrics.metrics import binary_metrics_runs_thresholds
+from mmu.metrics.metrics import col_index
+from mmu.metrics.metrics import col_names
+from mmu.metrics.metrics import confusion_matrix_to_dataframe
+from mmu.metrics.metrics import metrics_to_dataframe
+from mmu.lib._mmu_core import binary_metrics
+from mmu.lib._mmu_core import binary_metrics_proba
+from mmu.lib._mmu_core import binary_metrics_confusion
+from mmu.lib._mmu_core import binary_metrics_thresholds
+from mmu.lib._mmu_core import confusion_matrix
+from mmu.lib._mmu_core import confusion_matrix_proba
+
+__all__ = [
+    'binary_metrics',
+    'binary_metrics_proba',
+    'binary_metrics_confusion',
+    'binary_metrics_thresholds',
+    'binary_metrics_runs_thresholds',
+    'col_index',
+    'col_names',
+    'confusion_matrix_to_dataframe',
+    'confusion_matrix',
+    'confusion_matrix_proba',
+    'metrics_to_dataframe',
+]
