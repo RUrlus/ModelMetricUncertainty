6ccaae37d465032a70451ed6964841a82ccf613c
Ralph Urlus <rurlus.dev@gmail.com>
2021-12-02 21:30:19 +0100

    ENH: Add HDI functionality to metrics module

--- mmu/metrics/__init__.py
+++ mmu/metrics/__init__.py
@@ -2,6 +2,7 @@ from mmu.metrics.metrics import binary_metrics_runs_thresholds
 from mmu.metrics.metrics import col_index
 from mmu.metrics.metrics import col_names
 from mmu.metrics.metrics import confusion_matrix_to_dataframe
+from mmu.metrics.metrics import compute_hdi
 from mmu.metrics.metrics import metrics_to_dataframe
 from mmu.lib._mmu_core import binary_metrics
 from mmu.lib._mmu_core import binary_metrics_runs
@@ -23,5 +24,6 @@ __all__ = [
     'confusion_matrix_to_dataframe',
     'confusion_matrix',
     'confusion_matrix_proba',
+    'compute_hdi',
     'metrics_to_dataframe',
 ]
