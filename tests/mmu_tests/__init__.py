a629381f64e0d0fdff41894350f2e42343014436
Ralph Urlus <rurlus.dev@gmail.com>
2021-12-06 17:25:48 +0100

    TST: [CPP] Add build instructions to test package

--- tests/mmu_tests/__init__.py
+++ tests/mmu_tests/__init__.py
@@ -1,3 +1,4 @@
 from mmu_tests.utils import generate_test_labels
+from mmu_tests.lib import _mmu_core_tests
 
-__all__ = ['generate_test_labels']
+__all__ = ['generate_test_labels', '_mmu_core_tests']
