646f7e5c6ce57a7f187d20fbca30db75aad402e0
Ralph Urlus <rurlus.dev@gmail.com>
2021-12-06 19:26:08 +0100

    TST: Add reference metrics function for testing

--- tests/mmu_tests/__init__.py
+++ tests/mmu_tests/__init__.py
@@ -1,4 +1,5 @@
 from mmu_tests.utils import generate_test_labels
+from mmu_tests.utils import _compute_reference_metrics
 from mmu_tests.lib import _mmu_core_tests
 
-__all__ = ['generate_test_labels', '_mmu_core_tests']
+__all__ = ['_compute_reference_metrics', 'generate_test_labels', '_mmu_core_tests']
