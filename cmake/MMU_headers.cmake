07eb9a2cec89f2cb541216cceeba28878f8e9281
Ralph Urlus <rurlus.dev@gmail.com>
2021-12-10 17:05:01 +0100

    BLD: [CPP] Fixes linking to OpenMP when it was not found

--- cmake/MMU_headers.cmake
+++ cmake/MMU_headers.cmake
@@ -6,11 +6,19 @@ CMAKE_MINIMUM_REQUIRED(VERSION 3.16)
 ADD_LIBRARY(mmu_headers INTERFACE)
 ADD_LIBRARY(mmu::headers ALIAS mmu_headers)
 TARGET_INCLUDE_DIRECTORIES(mmu_headers INTERFACE ${MMU_HEADERS})
+
 TARGET_LINK_LIBRARIES(
     mmu_headers
     INTERFACE
         pybind11::pybind11
-        OpenMP::OpenMP_CXX
         Python3::NumPy
         Python3::Python
 )
+
+IF (OpenMP_CXX_FOUND)
+  TARGET_LINK_LIBRARIES(
+      mmu_headers
+      INTERFACE
+          OpenMP::OpenMP_CXX
+  )
+ENDIF ()
