f439e96b413364e9a90307a360977dc07dc7a592
Ralph Urlus <rurlus.dev@gmail.com>
2021-12-10 17:18:33 +0100

    BLD: [CPP] Do not link to Python3::Python but only include Python include dirs

--- cmake/MMU_headers.cmake
+++ cmake/MMU_headers.cmake
@@ -5,14 +5,17 @@ CMAKE_MINIMUM_REQUIRED(VERSION 3.16)
 ################################################################################
 ADD_LIBRARY(mmu_headers INTERFACE)
 ADD_LIBRARY(mmu::headers ALIAS mmu_headers)
-TARGET_INCLUDE_DIRECTORIES(mmu_headers INTERFACE ${MMU_HEADERS})
+TARGET_INCLUDE_DIRECTORIES(mmu_headers 
+    INTERFACE
+        ${MMU_HEADERS}
+        ${Python3_INCLUDE_DIRS}
+)
 
 TARGET_LINK_LIBRARIES(
     mmu_headers
     INTERFACE
         pybind11::pybind11
         Python3::NumPy
-        Python3::Python
 )
 
 IF (OpenMP_CXX_FOUND)
