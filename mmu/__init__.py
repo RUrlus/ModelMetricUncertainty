6b3cb359684721d153a72cd8566f58796e5e290f
Ralph Urlus <rurlus.dev@gmail.com>
2021-12-12 23:16:19 +0100

    CHG: Move Stan related functionality out to MMUR

--- mmu/__init__.py
+++ mmu/__init__.py
@@ -1,5 +1,4 @@
 import mmu.metrics as metrics
-import mmu.models as models
 
 from mmu.metrics import binary_metrics
 from mmu.metrics import binary_metrics_runs
@@ -13,12 +12,7 @@ from mmu.metrics import confusion_matrix_to_dataframe
 from mmu.metrics import metrics_to_dataframe
 from mmu.commons.utils import generate_data
 
-from mmu.models.beta_binomial import BetaBinomialConfusionMatrix
-from mmu.models.dirichlet_multinomial import DirichletMultinomialConfusionMatrix
-from mmu.models.dirichlet_multinomial import DirichletMultinomialMultiConfusionMatrix
-
 __all__ = [
-    'BetaBinomialConfusionMatrix',
     'binary_metrics',
     'binary_metrics_runs',
     'binary_metrics_proba',
@@ -28,10 +22,7 @@ __all__ = [
     'confusion_matrix',
     'confusion_matrix_proba',
     'confusion_matrix_to_dataframe',
-    'DirichletMultinomialConfusionMatrix',
-    'DirichletMultinomialMultiConfusionMatrix',
     'generate_data',
     'metrics',
     'metrics_to_dataframe',
-    'models',
 ]
