0691b9978ea2f1f8701a1a24be05a3cc72f6aa3e
Ralph Urlus <rurlus.dev@gmail.com>
2022-01-20 11:01:53 +0100

    CHG: [CPP] Give functions that directly interface with Numpy in own namespace

--- cpp/include/mmu/numpy.hpp
+++ cpp/include/mmu/numpy.hpp
@@ -20,64 +20,70 @@
 namespace py = pybind11;
 
 namespace mmu {
-namespace ccore {
+namespace npc {
 
-inline bool is_f_contiguous(const PyObject* src) {
+inline bool is_f_contiguous(PyObject* src) {
     return PyArray_CHKFLAGS(
-        reinterpret_cast<const PyArrayObject*>(src),
+        reinterpret_cast<PyArrayObject*>(src),
         NPY_ARRAY_F_CONTIGUOUS
     );
 }
 
-inline bool is_f_contiguous(const PyArrayObject* src) {
+inline bool is_f_contiguous(PyArrayObject* src) {
     return PyArray_CHKFLAGS(src, NPY_ARRAY_F_CONTIGUOUS);
 }
 
-inline bool is_c_contiguous(const PyObject* src) {
+inline bool is_c_contiguous(PyObject* src) {
     return PyArray_CHKFLAGS(
-        reinterpret_cast<const PyArrayObject*>(src),
+        reinterpret_cast<PyArrayObject*>(src),
         NPY_ARRAY_C_CONTIGUOUS
     );
 }
 
-inline bool is_c_contiguous(const PyArrayObject* src) {
+inline bool is_c_contiguous(PyArrayObject* src) {
     return PyArray_CHKFLAGS(src, NPY_ARRAY_C_CONTIGUOUS);
 }
 
-inline bool is_contiguous(const PyObject* src) {
-    auto arr = reinterpret_cast<const PyArrayObject*>(src);
+inline bool is_contiguous(PyObject* src) {
+    auto arr = reinterpret_cast<PyArrayObject*>(src);
+    auto obj_fields = reinterpret_cast<PyArrayObject_fields*>(arr);
     return (
         PyArray_CHKFLAGS(arr, NPY_ARRAY_C_CONTIGUOUS)
         || PyArray_CHKFLAGS(arr, NPY_ARRAY_F_CONTIGUOUS)
     );
 }
 
-inline bool is_contiguous(const PyArrayObject* arr) {
+inline bool is_contiguous(PyArrayObject* arr) {
     return (
         PyArray_CHKFLAGS(arr, NPY_ARRAY_C_CONTIGUOUS)
         || PyArray_CHKFLAGS(arr, NPY_ARRAY_F_CONTIGUOUS)
     );
 }
 
-}  // namespace ccore
-}  // namespace mmu
+inline bool is_well_behaved(PyObject* src) {
+    auto arr = reinterpret_cast<PyArrayObject*>(src);
+    return (
+        PyArray_CHKFLAGS(arr, NPY_ARRAY_ALIGNED | NPY_ARRAY_C_CONTIGUOUS)
+        || PyArray_CHKFLAGS(arr, NPY_ARRAY_ALIGNED | NPY_ARRAY_F_CONTIGUOUS)
+    );
+}
 
-namespace mmu {
-namespace details {
+}  // namespace npc
 
+namespace npy {
 template <typename T>
 inline bool is_f_contiguous(const py::array_t<T>& arr) {
-    return ccore::is_f_contiguous(arr.ptr());
+    return npc::is_f_contiguous(arr.ptr());
 }
 
 template <typename T>
 inline bool is_c_contiguous(const py::array_t<T>& arr) {
-    return ccore::is_c_contiguous(arr.ptr());
+    return npc::is_c_contiguous(arr.ptr());
 }
 
 template <typename T>
 inline bool is_contiguous(const py::array_t<T>& arr) {
-    return ccore::is_contiguous(arr.ptr());
+    return npc::is_contiguous(arr.ptr());
 }
 
 
@@ -102,7 +108,7 @@ inline py::array_t<T> allocate_confusion_matrix(const ssize_t n_matrices) {
     memset(reinterpret_cast<T*>(conf_mat.request().ptr), 0, n_matrices * block_size);
     return conf_mat;
 }
-}  // namespace details
+}  // namespace npy
 }  // namespace mmu
 
 #endif  // CPP_INCLUDE_MMU_NUMPY_HPP_
