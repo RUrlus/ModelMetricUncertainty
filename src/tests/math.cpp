902f74057da3ed530c27fdbc36bcb71ea22281ad
Ralph Urlus <rurlus.dev@gmail.com>
2022-05-04 17:31:21 +0200

    TST: [CPP] Add bindings for testing of multinomial_rvs

--- src/tests/math.cpp
+++ src/tests/math.cpp
@@ -26,4 +26,27 @@ void bind_norm_ppf(py::module &m) {
     );
 }
 
+void bind_multinomial_rvs(py::module &m) {
+    m.def(
+        "multinomial_rvs",
+        [](const int64_t n_samples, const int N, const py::array_t<double>& probas, const uint64_t seed) {
+            return multinomial_rvs<int>(n_samples, N, probas, seed);
+        },
+        py::arg("n_samples"),
+        py::arg("N"),
+        py::arg("probas"),
+        py::arg("seed")
+    );
+    m.def(
+        "multinomial_rvs",
+        [](const int64_t n_samples, const int64_t N, const py::array_t<double>& probas, const uint64_t seed) {
+            return multinomial_rvs<int64_t>(n_samples, N, probas, seed);
+        },
+        py::arg("n_samples"),
+        py::arg("N"),
+        py::arg("probas"),
+        py::arg("seed")
+    );
+}
+
 }  // namespace mmu_tests
