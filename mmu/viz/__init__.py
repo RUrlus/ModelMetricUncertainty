c98234d1f5c9d55f57c2e9a2539eb87e81f4bc6f
Ralph Urlus <rurlus.dev@gmail.com>
2022-05-18 09:05:44 +0200

    BLD: Fix import issue in CICD

---
+++ mmu/viz/__init__.py
@@ -0,0 +1,5 @@
+from mmu.viz.contours import _plot_pr_contours
+from mmu.viz.contours import _plot_pr_curve_contours
+from mmu.viz.ellipse import _plot_pr_ellipse
+
+__all__ = ['_plot_pr_contours', '_plot_pr_curve_contours', '_plot_pr_ellipse']
