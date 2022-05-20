1a9a1b5b03874a85a2e0aba1bc94b1926f4a9094
Ralph Urlus <rurlus.dev@gmail.com>
2022-05-20 16:07:36 +0200

    ENH: Add Elliptical uncertainty over PR curve

--- mmu/methods/__init__.py
+++ mmu/methods/__init__.py
@@ -1,15 +1,18 @@
 from mmu.methods.prpoint import PrecisionRecallEllipticalUncertainty
 from mmu.methods.prpoint import PrecisionRecallMultinomialUncertainty
 from mmu.methods.prcurve import PrecisionRecallCurveMultinomialUncertainty
+from mmu.methods.prcurve import PrecisionRecallCurveEllipticalUncertainty
 
 PREU = PrecisionRecallEllipticalUncertainty
 PRMU = PrecisionRecallMultinomialUncertainty
 PRCMU = PrecisionRecallCurveMultinomialUncertainty
+PRCEU = PrecisionRecallCurveEllipticalUncertainty
 
 __all__ = [
     'PREU',
     'PRMU',
     'PRCMU',
+    'PRCEU',
     'PrecisionRecallEllipticalUncertainty',
     'PrecisionRecallMultinomialUncertainty',
     'PrecisionRecallCurveMultinomialUncertainty',
