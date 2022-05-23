c5ea69560e4c804afacbc6e71734d6902c0521e5
Ralph Urlus <rurlus.dev@gmail.com>
2022-05-23 20:10:34 +0200

    DOC: Remove contents page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,6 +75,6 @@ html_show_sourcelink = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-master_doc = 'contents'
+master_doc = 'index'
 
 # -- Extension configuration -------------------------------------------------
