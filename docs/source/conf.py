a6c8a9605ffca9a6f7764ce44999be5c38c57265
Ralph Urlus <rurlus.dev@gmail.com>
2022-07-22 14:58:30 +0200

    REL: mmu v0.1.0rc4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ copyright = '2022, R. Urlus'
 author = 'R. Urlus'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0rc3'
+release = '0.1.0rc4'
 
 
 # -- General configuration ---------------------------------------------------
