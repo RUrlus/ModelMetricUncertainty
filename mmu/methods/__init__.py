c20777bcf12b3a142645420b1eaac8f69b8a6b87
Ralph Urlus <rurlus.dev@gmail.com>
2026-05-21 10:28:11 +0200

    FEAT: Pure Python implementation of joint uncertainty over PPN and Recall

--- mmu/methods/__init__.py
+++ mmu/methods/__init__.py
@@ -4,22 +4,29 @@ from mmu.methods.prcurve import PrecisionRecallCurveUncertainty
 from mmu.methods.rocpoint import ROCUncertainty
 from mmu.methods.rocpoint import ROCSimulatedUncertainty
 from mmu.methods.roccurve import ROCCurveUncertainty
+from mmu.methods.recall_ppn import RecallPPNUncertainty
+from mmu.methods.recall_ppn import RecallPPNCurveUncertainty
 
 PRU = PrecisionRecallUncertainty
 PRCU = PrecisionRecallCurveUncertainty
 ROCU = ROCUncertainty
 ROCCU = ROCCurveUncertainty
+RPPNU = RecallPPNUncertainty
+RPPNCU = RecallPPNCurveUncertainty
 
 __all__ = [
     "PRU",
     "PRCU",
+    "RPPNU",
+    "RPPNCU",
     "PrecisionRecallUncertainty",
     "PrecisionRecallSimulatedUncertainty",
     "PrecisionRecallCurveUncertainty",
-
     "ROCU",
     "ROCCU",
     "ROCUncertainty",
     "ROCSimulatedUncertainty",
     "ROCCurveUncertainty",
+    "RecallPPNUncertainty",
+    "RecallPPNCurveUncertainty",
 ]
