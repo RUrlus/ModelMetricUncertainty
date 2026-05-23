1414c58588b6cc84469394c669a2c0fd4aaba428
Ralph Urlus <rurlus.dev@gmail.com>
2026-05-23 22:10:17 +0200

    STYLE: String formatting

--- src/mmu/viz/utils.py
+++ src/mmu/viz/utils.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import rgb2hex
 from matplotlib.lines import Line2D
 from matplotlib.patches import Patch
-import matplotlib.cm as plt_cm
 
 
 def _set_plot_style(get_colors=False):
@@ -20,24 +19,24 @@ def _get_color_hexes(
         raise TypeError("`cmap` must be a str")
 
     t_colors = n_colors + 2 * n_offset + 1
-    cmap = plt_cm.get_cmap(cmap, t_colors)
+    cmap = plt.get_cmap(cmap, t_colors)
     hexes = [rgb2hex(cmap(i), keep_alpha=keep_alpha) for i in range(cmap.N)]
     if return_marker:
         return hexes[n_offset : n_offset + n_colors][::-1], hexes[-3]
     return hexes[n_offset : n_offset + n_colors][::-1]
 
 
-def _create_pr_legend(c_hexes, labels, y_label='Precision', x_label='Recall'):
+def _create_pr_legend(c_hexes, labels, y_label="Precision", x_label="Recall"):
     # Map metric names to their LaTeX symbols
     label_to_symbol = {
-        'Precision': r'\hat{P}',
-        'Recall': r'\hat{R}',
-        'TPR': r'\widehat{TPR}',
-        'FPR': r'\widehat{FPR}',
-        'PPN': r'\widehat{PPN}',
+        "Precision": r"\hat{P}",
+        "Recall": r"\hat{R}",
+        "TPR": r"\widehat{TPR}",
+        "FPR": r"\widehat{FPR}",
+        "PPN": r"\widehat{PPN}",
     }
-    y_sym = label_to_symbol.get(y_label, rf'\widehat{{{y_label}}}')
-    x_sym = label_to_symbol.get(x_label, rf'\widehat{{{x_label}}}')
+    y_sym = label_to_symbol.get(y_label, rf"\widehat{{{y_label}}}")
+    x_sym = label_to_symbol.get(x_label, rf"\widehat{{{x_label}}}")
     curve_label = rf"${x_sym}, {y_sym}$"
 
     line = [Line2D([0], [0], color="black", alpha=0.6, label=curve_label)]
@@ -47,17 +46,19 @@ def _create_pr_legend(c_hexes, labels, y_label='Precision', x_label='Recall'):
     return line + patches  # type: ignore
 
 
-def _create_pr_legend_scatter(c_hexes, c_marker, labels, vals, y_label='Precision', x_label='Recall'):
+def _create_pr_legend_scatter(
+    c_hexes, c_marker, labels, vals, y_label="Precision", x_label="Recall"
+):
     # Map metric names to their LaTeX symbols
     label_to_symbol = {
-        'Precision': r'\hat{P}',
-        'Recall': r'\hat{R}',
-        'TPR': r'\widehat{TPR}',
-        'FPR': r'\widehat{FPR}',
-        'PPN': r'\widehat{PPN}',
+        "Precision": r"\hat{P}",
+        "Recall": r"\hat{R}",
+        "TPR": r"\widehat{TPR}",
+        "FPR": r"\widehat{FPR}",
+        "PPN": r"\widehat{PPN}",
     }
-    y_sym = label_to_symbol.get(y_label, rf'\widehat{{{y_label}}}')
-    x_sym = label_to_symbol.get(x_label, rf'\widehat{{{x_label}}}')
+    y_sym = label_to_symbol.get(y_label, rf"\widehat{{{y_label}}}")
+    x_sym = label_to_symbol.get(x_label, rf"\widehat{{{x_label}}}")
 
     if vals:
         label = (
